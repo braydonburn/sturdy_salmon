56c1fd0aebd8a940fbb28801110412f16b719e44
Braydon <info@braydonburn.com>
2018-04-02 20:13:18 +1000

    Added form validation for login page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,25 @@ function getLocation() {
     searchField.value = position.coords.latitude + " " + position.coords.longitude;
   }
 }
+
+function validate() {
+  validateEmail();
+  validatePassword();
+}
+
+
+function validateEmail() {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.forms["myForm"]["email"].value)) {
+    return(true);
+  }
+  document.getElementById("emailMissing").style.visibility = "visible";
+  return false;
+}
+
+function validatePassword() {
+  if ((document.forms["myForm"]["password"].value) === "") {
+    document.getElementById("passwordMissing").style.visibility = "visible";
+    return(false);
+  }
+  return true;
+}

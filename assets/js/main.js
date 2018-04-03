ab074af4a168bb5bbebe2b19126787fd84a7aa17
Braydon <info@braydonburn.com>
2018-04-03 20:08:30 +1000

    Added comments to JS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,15 +22,17 @@ function validateRegistration() {
   validateDate();
 }
 
-
+//Validates email using regex expressions
 function validateEmail() {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.forms["myForm"]["email"].value)) {
     return(true);
   }
+  //Displays error message if test fails
   document.getElementById("emailMissing").style.visibility = "visible";
   return false;
 }
 
+//Checks if password is empty, displays error if not
 function validatePassword() {
   if ((document.forms["myForm"]["password"].value) === "") {
     document.getElementById("passwordMissing").style.visibility = "visible";
@@ -43,12 +45,14 @@ function validateDate() {
   var x = true;
   var day = document.forms["myForm"]["day"].value;
   var year = document.forms["myForm"]["year"].value;
+  //Uses if or statements to get if date is correct
   if ((isNaN(day)) || (day > 31) || (day <= 0) || (isNaN(year)) || (year > 2018) || (year < 1900)) {
     document.getElementById("dateMissing").style.visibility = "visible";
     x = false;
   }
 }
 
+//Various if statements to check if values are empty
 function validateEmpty() {
   var x = true;
   if ((document.forms["myForm"]["fullName"].value) === "") {

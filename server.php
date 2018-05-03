3f0cb576c7fffcafcc0c8ed2dfa5b061756e514d
Braydon <info@braydonburn.com>
2018-05-03 19:53:20 +1000

    fixed untracked files

--- server.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Host, username, password, database below.
-$con = mysqli_connect("localhost","root","dreadman1","cab230");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>

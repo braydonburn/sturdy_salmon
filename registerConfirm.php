2ac0d2151ccd06301c8bc638e71fbe9325dafd21
Braydon <info@braydonburn.com>
2018-05-06 18:38:45 +1000

    First attempt at getting registration to work with mysql.

--- registerConfirm.php
+++ registerConfirm.php
@@ -4,11 +4,26 @@
         #require 'assets/php/validate.php';
         #validateEmail($errors, $_POST, 'email');
         if ($errors) {
+            echo "<h3>Error1.</h3>";
             include 'registrationPage.php';
         } else {
             #echo 'Form submitted successfully with no errors';
-            include 'registrationPage.php';
+            $fullname = stripslashes($_REQUEST['fullname']);
+            $fullname = mysqli_real_escape_string($con, $fullname);
+            $username = stripslashes($_REQUEST['username']);
+            $username = mysqli_real_escape_string($con, $username);
+            $email = stripslashes($_REQUEST['email']);
+            $email = mysqli_real_escape_string($con, $email);
+            $password = stripslashes($_REQUEST['password']);
+            $password = mysqli_real_escape_string($con, $password);
+
+            $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$fullname', '$username', '$email', '".md5($password)."')";
+            $result = mysqli_query($con, $query);
+            if ($result) {
+                echo "<h3>You are registered successfully.</h3>";
+            }
         }
     } else {
+        echo "<h3>Error2.</h3>";
         include 'registrationPage.php';
     }

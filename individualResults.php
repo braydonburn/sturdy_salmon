26cc22585cfde528e691dc0cde3114cea159689e
Bertrand Dungan <bertranddungan@gmail.com>
2018-05-26 11:02:20 +1000

    Changed to prepared statement and made quotation marks consistent

--- individualResults.php
+++ individualResults.php
@@ -1,32 +1,31 @@
 <!DOCTYPE HTML>
-<html lang="en">
+<html lang='en'>
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="author" content="Braydon Burn & Bertrand Dungan">
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <meta name='author' content='Braydon Burn & Bertrand Dungan'>
   <title>Search</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="assets/css/main.css">
-  <script src="assets/js/main.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,700" rel="stylesheet">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+  <link rel='stylesheet' href='assets/css/main.css'>
+  <script src='assets/js/main.js'></script>
+  <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,600,700' rel='stylesheet'>
 
   <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-  <link rel="manifest" href="assets/favicon/site.webmanifest">
-  <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
+  <link rel='apple-touch-icon' sizes='180x180' href='assets/favicon/apple-touch-icon.png'>
+  <link rel='icon' type='image/png' sizes='32x32' href='assets/favicon/favicon-32x32.png'>
+  <link rel='icon' type='image/png' sizes='16x16' href='assets/favicon/favicon-16x16.png'>
+  <link rel='manifest' href='assets/favicon/site.webmanifest'>
+  <link rel='mask-icon' href='assets/favicon/safari-pinned-tab.svg' color='#5bbad5'>
+  <meta name='msapplication-TileColor' content='#da532c'>
+  <meta name='theme-color' content='#ffffff'>
 </head>
 
 <body>
   <!-- Header template -->
   <?php
   include('assets/php/functions.php');
-  include('server.php');
   genHead();
 
   if (isset($_GET['id'])) {
@@ -35,18 +34,18 @@
 
   $output = '';
 
-  $query = "SELECT *
-            FROM Items
-            WHERE id
-            LIKE '%$id%'";
-  $result = mysqli_query($con, $query) or die(mysqli_error());
+  $pdo = new PDO('mysql:host=localhost;dbname=cab230', 'root1', 'password');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $count = mysqli_num_rows($result);
+  $query = $pdo->prepare('SELECT * FROM Items WHERE id = :id');
+  $query->bindvalue(':id',$id);
+  $query->execute();
+  $count = $query->rowCount();
 
   if ($count == 0) {
       $output = 'There was an error finding the result you searched for, sorry.';
   } else {
-      while ($row = mysqli_fetch_array($result)) {
+      while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
           $hotspotName = $row['hotspotName'];
           $address = $row['address'];
           $suburb = $row['suburb'];
@@ -57,15 +56,15 @@
   ?>
   <!-- End Header template -->
 
-  <script type="text/javascript">
+  <script type='text/javascript'>
   // This function creates the Google map to show wifi locations
   //Pass php lat/long variables to js
-  var latitude = "<?php echo $latitude; ?>";
-  var longitude = "<?php echo $longitude; ?>";
+  var latitude = '<?php echo $latitude; ?>';
+  var longitude = '<?php echo $longitude; ?>';
 
   function myMap() {
     var myCenter = new google.maps.LatLng(latitude, longitude);
-    var mapCanvas = document.getElementById("map");
+    var mapCanvas = document.getElementById('map');
     var mapOptions = {
       center: myCenter,
       zoom: 18
@@ -79,39 +78,39 @@
   </script>
 
   <!-- Individual results form with reviews -->
-    <div class="font resultName">
+    <div class='font resultName'>
       <h2><?php print("$hotspotName"); ?></h2>
-      <div id="map">
+      <div id='map'>
       </div>
       <h1><?php print("$output"); ?></h1>
-      <div class="grid-container">
+      <div class='grid-container'>
 
-        <div class="review-left">
-          <div class="font">
+        <div class='review-left'>
+          <div class='font'>
             <h2>Address</h2>
             <p><?php print("$address"); ?></p>
             <h2>Reviews</h2>
           </div>
 
-          <div class="review">
+          <div class='review'>
             <h3>David: ★ ★ ★ ★ ☆</h3>
             <p>Easy to connect to and decent speed.</p>
           </div>
-          <div class="review">
+          <div class='review'>
             <h3>Lauren: ★ ★ ★ ☆ ☆</h3>
             <p>Speed could be better.</p>
           </div>
-          <div class="review">
+          <div class='review'>
             <h3>Jason: ★ ★ ★ ★ ★</h3>
             <p>Easy connection and fast speed.</p>
           </div>
-          <div class="review">
+          <div class='review'>
             <h3>Libby: ★ ★ ☆ ☆ ☆</h3>
             <p>Terrible speed but easy to connect to.</p>
           </div>
         </div>
 
-      <form method="post" action="reviewPost.php">
+      <form method='post' action='reviewPost.php'>
         <?php showReviewBox() ?>
       </form>
 
@@ -119,12 +118,12 @@
   </div>
 
   <!-- Footer template -->
-  <footer class="footer font">
+  <footer class='footer font'>
     <a>© 2018 Braydon Burn & Bertrand Dungan</a>
   </footer>
   <!-- End Footer template -->
 
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC0n5agCie-72j_C-hrl8ByvMjDv5J23zk&callback=myMap"></script>
+  <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC0n5agCie-72j_C-hrl8ByvMjDv5J23zk&callback=myMap'></script>
 </body>
 
 

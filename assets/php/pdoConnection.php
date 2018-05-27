5c439408cee29bce310017f8b61787823a82f015
Bertrand Dungan <bertranddungan@gmail.com>
2018-05-27 21:16:04 +1000

    Readded PDO as comment that will be used for final production

--- assets/php/pdoConnection.php
+++ assets/php/pdoConnection.php
@@ -1,5 +1,6 @@
 <?PHP
 $pdo = new PDO('mysql:host=localhost;dbname=cab230', 'root', 'dreadman1');
+#$pdo = new PDO('mysql:host=localhost;dbname=n9715894', 'n9715894', 'password');
 #$pdo = new PDO('mysql:host=localhost;dbname=cab230', 'root1', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>

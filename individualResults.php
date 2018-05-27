b9898a5d8ea89db99c465873fa392791156d0da5
Bertrand Dungan <bertranddungan@gmail.com>
2018-05-27 22:09:31 +1000

    Added Microdata

--- individualResults.php
+++ individualResults.php
@@ -33,8 +33,6 @@
       $id = $_GET['id'];
   }
 
-  $output = '';
-
   # This querys the database for the location details relating to the hotspot
   require('assets/php/pdoConnection.php');
   $query = $pdo->prepare('SELECT * FROM Items WHERE id = :id');
@@ -85,15 +83,22 @@
       <h2><?php print("$hotspotName"); ?></h2>
       <div id='map'>
       </div>
-      <h1><?php print("$output"); ?></h1>
       <div class='grid-container'>
-
         <div class='review-left'>
-          <div class='font'>
+          <!-- This  shows the user the address of the wifi with the suburb and
+        the GeoCoordinates. It also has microdata for all of these items-->
+          <div class='font' itemscope itemtype='http://schema.org/Place'>
             <h2>Address</h2>
-            <p><?php print("$address"); ?></p>
-            <h2>Reviews</h2>
+            <span itemprop="streetAddress"><p><?php print("$address"); ?></p></span>
+            <span itemprop="addressLocality"><p><?php print("$suburb"); ?></p></span>
+            <div itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+              <p>Latitude: <?php print("$latitude"); ?></p>
+              <p>Longitude: <?php print("$longitude"); ?></p>
+              <meta itemprop="latitude" content=<?php print("$latitude"); ?>/>
+              <meta itemprop="longitude" content=<?php print("$longitude"); ?>/>
+            </div>
           </div>
+          <h2>Reviews</h2>
 
           <?php
           # This shows the reviews that other people have posted with their
@@ -104,7 +109,9 @@
           $query->execute();
           $count = $query->rowCount();
 
-          # This checks if there are any reviews yet
+          # This checks if there are any reviews yet, if there aren't it shows a
+          #message, otherwise it will show each review with its star rating, the
+          #author, and the date it was published. These all have microdata.
           if ($count == 0) {
             echo '<h2>There are no reviews for this wifi yet</h2>';
           } else {
@@ -121,15 +128,17 @@
                   }
                   $comment = sanitise($row['comment']);
                   $date = $row['date'];
-                  $reviewContents .= '<div class=\'review\'><h3>'.$username.': '
-                  .$ratingStar.'</h3>'.'<p>'.$comment.'</p><p>'.
-                  $date.'</p></div>';
+                  $reviewContents .= '<div class=\'review\' itemscope
+                  itemtype=\'http://schema.org/Review\'><h3><span
+                  itemprop=\'author\'>'.$username.'</span>: <span
+                  itemprop=\'reviewRating\'>'.$ratingStar.'</span></h3>'.
+                  '<p><span itemprop=\'description\'>'.$comment.'</span></p><p>
+                  <span itemprop=\'datePublished\'>'.$date.'</span></p></div>';
               }
               print("$reviewContents");
           }
            ?>
         </div>
-
         <!-- This shows an box where users can add reviews, but it is only
         visible if the user is logged in -->
       <form method='post' action='reviewPost.php'>
@@ -144,7 +153,6 @@
     <a>© 2018 Braydon Burn & Bertrand Dungan</a>
   </footer>
   <!-- End Footer template -->
-
   <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC0n5agCie-72j_C-hrl8ByvMjDv5J23zk&callback=myMap'></script>
 </body>
 </html>

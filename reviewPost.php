8a8d2b857473f6f2d43d942faf0a7b3f0145b964
Bertrand Dungan <bertranddungan@gmail.com>
2018-05-27 11:57:47 +1000

    Made review posting safer and added comments. Also removed error pages in favour of redirects

--- reviewPost.php
+++ reviewPost.php
@@ -1,31 +1,42 @@
 <?php
 session_start();
+require('assets/php/validation.php');
+    # This will redirect the user back to the login page if they are not logged in
     if (isset($_SESSION['username'])) {
       $errors = array();
+
+      # This will return users to the search page if they did not get here
+      #through the review form
       if (!empty($_POST)) {
         date_default_timezone_set('Australia/Brisbane');
         $hotspotID = $_POST['id'];
-        $username = $_SESSION['username'];
+        $username = ($_SESSION['username']);
         $date = date('Y-m-d');
-        $comment = $_POST['comment'];
+        $comment = sanitise($_POST['comment']);
         $rating = $_POST['rating'];
 
+          # This will insert the review into the database if there are no errors
+          #and then it will redirect them to the page they just reviewed
           if (count($errors)>0) {
-              echo "<h3>Error1.</h3>";
+            header('location: individualResults.php?id='.$hotspotID);
           } else {
-            $pdo = new PDO('mysql:host=localhost;dbname=cab230', 'root1', 'password');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $query = $pdo->prepare('INSERT INTO Reviews (hotspotID, username, date, comment, rating) VALUES (:hotspotID, :username, :date, :comment, :rating)');
+            $pdo = new PDO('mysql:host=localhost;dbname=cab230', 'root1',
+            'password');
+            $query = $pdo->prepare('INSERT INTO Reviews (hotspotID, username,
+              date, comment, rating) VALUES (:hotspotID, :username, :date,
+                :comment, :rating)');
             $query->bindvalue(':hotspotID', $hotspotID);
             $query->bindvalue(':username', $username);
             $query->bindvalue(':date', $date);
             $query->bindvalue(':comment', $comment);
             $query->bindvalue(':rating', $rating);
             $query->execute();
+            header('location: individualResults.php?id='.$hotspotID);
           }
       } else {
-          echo "<h3>Error2.</h3>";
+        header('location: searchPage.php');
       }
     } else {
         header('location: loginPage.php');
     }
+?>
